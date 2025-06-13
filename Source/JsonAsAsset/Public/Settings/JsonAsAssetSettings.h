7e22a468364d2783c4e3ff519bdf2be91313bf8e
Tectors <73559984+Tectors@users.noreply.github.com>
2025-06-13 00:55:08 -0700

    Update JsonAsAssetSettings.h

--- Source/JsonAsAsset/Public/Settings/JsonAsAssetSettings.h
+++ Source/JsonAsAsset/Public/Settings/JsonAsAssetSettings.h
@@ -141,7 +141,7 @@ public:
 };
 
 /* A user-friendly Unreal Engine plugin designed to import assets from packaged games through JSON files */
-UCLASS(Config = EditorPerProjectUserSettings)
+UCLASS(Config = EditorPerProjectUserSettings, DefaultConfig)
 class JSONASASSET_API UJsonAsAssetSettings : public UDeveloperSettings {
 	GENERATED_BODY()
 

055321cb874fd26672b35c4fbbd3d5cf421a3b68
Tectors <73559984+Tectors@users.noreply.github.com>
2025-07-11 22:07:41 -0700

    Remove Game Specific AES Fetching

--- Source/JsonAsAsset/Private/Settings/Details/JsonAsAssetSettingsDetails.cpp
+++ Source/JsonAsAsset/Private/Settings/Details/JsonAsAssetSettingsDetails.cpp
@@ -11,7 +11,6 @@
 #include "Serialization/JsonSerializer.h"
 #include "Misc/FileHelper.h"
 #include "Dom/JsonObject.h"
-#include "HttpModule.h"
 #include "Modules/LocalFetchModule.h"
 #include "Utilities/EngineUtilities.h"
 
@@ -25,94 +24,11 @@ TSharedRef<IDetailCustomization> FJsonAsAssetSettingsDetails::MakeInstance() {
 	return MakeShareable(new FJsonAsAssetSettingsDetails);
 }
 
-void FJsonAsAssetSettingsDetails::InitializeAESKeys(UJsonAsAssetSettings* PluginSettings) {
-	const TSharedPtr<FJsonObject> AES = RequestObjectURL("https://fortnitecentral.genxgames.gg/api/v1/aes");
-
-	if (!AES.IsValid()) return;
-	
-	PluginSettings->DynamicKeys.Empty();
-	PluginSettings->ArchiveKey = AES->GetStringField(TEXT("mainKey"));
-	
-	for (const TSharedPtr<FJsonValue> Value : AES->GetArrayField(TEXT("dynamicKeys"))) {
-		const TSharedPtr<FJsonObject> Object = Value->AsObject();
-
-		const FString GUID = Object->GetStringField(TEXT("guid"));
-		const FString Key = Object->GetStringField(TEXT("key"));
-
-		PluginSettings->DynamicKeys.Add(FLocalFetchAES(GUID, Key));
-	}
-
-	SavePluginConfig(PluginSettings);
-}
-
-void FJsonAsAssetSettingsDetails::InitializeMappings(const UJsonAsAssetSettings* PluginSettings) {
-	FString LocalExportDirectory = PluginSettings->ExportDirectory.Path;
-
-	if (LocalExportDirectory != "" && LocalExportDirectory.Contains("/")) {
-		FString DataFolder; {
-			if (LocalExportDirectory.EndsWith("/")) {
-				LocalExportDirectory.Split("/", &DataFolder, nullptr, ESearchCase::IgnoreCase, ESearchDir::FromEnd);
-			} else {
-				DataFolder = LocalExportDirectory;
-			}
-
-			DataFolder.Split("/", &DataFolder, nullptr, ESearchCase::IgnoreCase, ESearchDir::FromEnd); {
-				DataFolder = DataFolder + "/.data";
-			}
-		}
-
-		const TArray<TSharedPtr<FJsonValue>> MappingsArray = RequestArrayURL("https://fortnitecentral.genxgames.gg/api/v1/mappings");
-		
-		TSharedPtr<FJsonValue> MappingsValue; {
-			if (MappingsArray.IsValidIndex(1)) {
-				MappingsValue = MappingsArray[1];
-			} else {
-				MappingsValue = MappingsArray[0];
-			}
-		}
-
-		if (MappingsValue == nullptr) return;
-		
-		const TSharedPtr<FJsonObject> MappingsObject = MappingsValue->AsObject();
-
-		FString FileName = MappingsObject->GetStringField(TEXT("fileName"));
-
-		auto OnRequestComplete = [DataFolder, FileName](FHttpRequestPtr, const FHttpResponsePtr& Response, const bool bWasSuccessful) {
-			if (bWasSuccessful) {
-				UJsonAsAssetSettings* ReferenceSettings = GetMutableDefault<UJsonAsAssetSettings>();
-
-				FFileHelper::SaveArrayToFile(Response->GetContent(), *(DataFolder + "/" + FileName));
-				ReferenceSettings->MappingFilePath.FilePath = DataFolder + "/" + FileName;
-				
-				SavePluginConfig(ReferenceSettings);
-			}
-		};
-
-		const TSharedRef<IHttpRequest, ESPMode::ThreadSafe> MappingsRequest = FHttpModule::Get().CreateRequest();
-		MappingsRequest->SetVerb("GET");
-		
-		const FString URL = MappingsObject->GetStringField(TEXT("url"));
-		MappingsRequest->SetURL(URL);
-		
-		MappingsRequest->OnProcessRequestComplete().BindLambda(OnRequestComplete);
-		MappingsRequest->ProcessRequest();
-	}
-}
-
 void FJsonAsAssetSettingsDetails::CustomizeDetails(IDetailLayoutBuilder& DetailBuilder) {
 	TArray<TWeakObjectPtr<UObject>> ObjectsBeingCustomized;
 	DetailBuilder.GetObjectsBeingCustomized(ObjectsBeingCustomized);
 
-	/* Reference to settings */
-	const TWeakObjectPtr<UJsonAsAssetSettings> Settings = Cast<UJsonAsAssetSettings>(ObjectsBeingCustomized[0].Get());
-
 	EditConfiguration(DetailBuilder);
-
-	DetailBuilder.EditCategory("Local Fetch", FText::GetEmpty(), ECategoryPriority::Important);
-	DetailBuilder.EditCategory("Local Fetch - Configuration", FText::GetEmpty(), ECategoryPriority::Important);
-	DetailBuilder.EditCategory("Local Fetch - Encryption", FText::GetEmpty(), ECategoryPriority::Important);
-
-	EditEncryption(Settings, DetailBuilder);
 }
 
 // ReSharper disable once CppMemberFunctionMayBeConst
@@ -196,38 +112,6 @@ void FJsonAsAssetSettingsDetails::EditConfiguration(IDetailLayoutBuilder& Detail
             return FReply::Handled();
         })
     ];
-}
-
-// ReSharper disable once CppMemberFunctionMayBeConst
-void FJsonAsAssetSettingsDetails::EditEncryption(TWeakObjectPtr<UJsonAsAssetSettings> Settings, IDetailLayoutBuilder& DetailBuilder) {
-	IDetailCategoryBuilder& EncryptionCategory = DetailBuilder.EditCategory("Local Fetch - Encryption", FText::GetEmpty(), ECategoryPriority::Important);
-	DetailBuilder.EditCategory("Local Fetch - Director", FText::GetEmpty(), ECategoryPriority::Important);
-
-	EncryptionCategory.AddCustomRow(LOCTEXT("EncryptionKeyFetcher", "EncryptionKeyFetcher"))
-		.NameContent()
-		[
-			/* This defines the name/title of the row */
-			SNew(STextBlock)
-				.Text(LOCTEXT("UseFModelAppSettings", "Fetch Encryption from a API"))
-				.Font(IDetailLayoutBuilder::GetDetailFont())
-		]
-		.ValueContent()[
-		SNew(SButton)
-		.Text(LOCTEXT("FetchEncryptionKey", "Fortnite Central API"))
-		.ToolTipText(LOCTEXT("FetchEncryptionKey_Tooltip", "Retrieves encryption keys from the Fortnite Central API"))
-		.OnClicked_Lambda([this]
-		{
-			UJsonAsAssetSettings* PluginSettings = GetMutableDefault<UJsonAsAssetSettings>();
-
-			InitializeAESKeys(PluginSettings);
-			InitializeMappings(PluginSettings);
-
-			return FReply::Handled();
-		}).IsEnabled_Lambda([this, Settings]()
-		{
-			return Settings->bEnableLocalFetch;
-		})
-	];
 }
 
 #undef LOCTEXT_NAMESPACE
